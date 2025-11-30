4f69eeb7eebd21df801339ae809ff96e83bb957e
Joet桀 <zoexml@gmail.com>
2025-11-30 17:00:49 +0800

    ci: 验证commitlint规则是否生效

---
+++ packages/lint-configs/commitlint-config/build.config.ts
@@ -0,0 +1,10 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  // 每次构建之前，输出目录（outDir）将被清空
+  clean: true,
+  //  自动生成 .d.ts 类型声明
+  declaration: true,
+  // 指定要打包的入口文件
+  entries: ['src/index'],
+})
