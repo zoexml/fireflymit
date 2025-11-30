fe255ab2236e2dfeec8793d68df2ab0e9001a2fe
Joet桀 <zoexml@gmail.com>
2025-11-30 15:27:31 +0800

    feat: ✨ 添加 Prettier 配置文件以定义代码格式化规则

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+// prettier.config.js
+/**
+ * @type {import('prettier').Config}
+ * @see https://www.prettier.cn/docs/options.html
+ */
+
+// 使用import来加载共享配置
+import baseConfig from '@fireflymit/prettier-config'
+
+// Prettier配置对象，用于定义代码格式化规则
+const config = {
+  ...baseConfig,
+  printWidth: 120,
+}
+
+// 导出配置对象以供Prettier使用
+export default config
